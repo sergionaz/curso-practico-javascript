19051bf456e31dda33ded45c4751329d85150035
Sergio Nazar <nsergio.nazar@gmail.com>
2022-08-24 11:21:01 -0300

    Guardando más cambios

--- 03-segundo-curso-practico/script3.js
+++ 03-segundo-curso-practico/script3.js
@@ -4,9 +4,24 @@ const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
+const form = document.querySelector('#form');
+// form.addEventListener('submit', sumarInputValues);
 
-btn.addEventListener('click', btnOnClick);
+/* Opción 1: Con click */
+// btn.addEventListener('click', onClickButton);
+// function onClickButton() {
+//     pResult.innerText = Number(input1.value)+ Number(input2.value);
+// }
 
-function btnOnClick() {
-    pResult.innerText = Number(input1.value)+ Number(input2.value)
+/* Opción 2: Con formulario */
+// form.addEventListener('submit', sumarInputValues);
+// function sumarInputValues(event) {
+//     event.preventDefault();
+//     pResult.innerText = Number(input1.value)+ Number(input2.value);
+// }
+
+/* Opción 3: con formulario, pero específicando al botón que es de tipo button*/ 
+btn.addEventListener('click', onClickButton);
+function onClickButton() {
+    pResult.innerText = Number(input1.value)+ Number(input2.value);
 }
