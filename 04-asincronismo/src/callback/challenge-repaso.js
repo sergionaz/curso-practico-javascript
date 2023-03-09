405b8291d8cf6cc189fe6292e42b38f7fd0782ee
Sergio Nazar <nsergio.nazar@gmail.com>
2023-03-09 10:10:01 -0300

    Cierro asincronismo - Callback

--- 04-asincronismo/src/callback/challenge-repaso.js
+++ 04-asincronismo/src/callback/challenge-repaso.js
@@ -25,8 +25,8 @@ fetchData(`${API}/products`, function (error1, data1) {
         fetchData(`${API}/categories/${data2.category.id}`, function (error3, data3) {
             if (error3) return console.error(error3);
             console.log(data1[0]);
-            console.log(data2.title);
-            console.log(data3.name);
+            console.log(`Nombre del producto: ${data2.title}`);
+            console.log(`Categoría: ${data3.name}`);
         });
     });
 });
